3b7305701c35fa31018d9a8c15c6fd4022089b94
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-29 13:10:48 -0700

    pass titleize test after minimum code added.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  before_save(:titleize_product)
+
+  private 
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
