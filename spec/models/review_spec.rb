8778fc25f2ddafbfb149dc436b58dfb0e42386b0
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-29 13:32:44 -0700

    add validation test for max length of content body, expect to fail.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,5 @@ describe Review do
   it {should validate_presence_of :author}
   it {should validate_presence_of :content_body}
   it {should validate_presence_of :rating}
+  it { should validate_length_of(:content_body).is_at_most(250)}
 end
