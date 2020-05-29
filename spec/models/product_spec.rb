f9748c870817319b8971a3d8e503f9859c6e2dc8
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-29 13:05:37 -0700

    add test for titleize method, expect to fail.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Product do
   it{ should have_many(:reviews) }
+
+  it("titleizes the name of an product") do
+    product = Product.create({name: "dragon fruit"})
+    expect(product.name()).to(eq("Dragon Fruit"))
+  end
 end
