a8aff81c2197313e7ba0996ded4e1da028999f84
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-29 14:34:58 -0700

    seed the database with 50 products, and 12499 reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,6 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content_body, presence: true
   # didn't think I needed to test the rating between a number of 1 - 5 since the radio buttons limit that already.
-  validates :rating, presence: true
   validates_length_of :content_body, maximum: 250
   validates_length_of :content_body, minimum: 50
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Product.destroy_all
+
+50.times do |products_index|
+  product = Product.create!(name: Faker::Food.fruits,
+                          cost: Faker::Number.number(digits: 2),
+                          country_of_origin: Faker::Nation.capital_city)
+  250.times do
+    review = product.reviews.new(author: Faker::FunnyName.name,
+                            content_body: Faker::Hipster.sentence(word_count: 15))
+                            review.save
+  end
+end
+p "Created #{Review.count} reviews"
+p "Created #{Product.count} products"
+
