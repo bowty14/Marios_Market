1f74039280fd007c3993ac7b9a4305df9c614150
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-06-05 10:00:56 -0700

    add a users table to database, create a user model for encrypting passwords.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_155142) do
+ActiveRecord::Schema.define(version: 2020_06_05_165104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2020_05_29_155142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
   end
 
   add_foreign_key "reviews", "products"
