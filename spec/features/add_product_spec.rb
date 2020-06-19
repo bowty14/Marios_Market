8ec22f0b2460b180364b0b0249fdd8a58f5969d4
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-06-19 16:49:11 -0700

    add_product spec now creates a new user before loggin in. fixing an error where all tests were not passing.

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
+user = User.create({email: "y@y.com", password: "Qwerty1", password_confirmation: "Qwerty1", admin: "t"})
 
 describe "the add a product process" do
   it "adds a new product" do
     visit signin_path
-    fill_in :email, :with => "t@t.com"
-    fill_in "Password", :with => "Venom9314"
+    fill_in :email, :with => "y@y.com"
+    fill_in "Password", :with => "Qwerty1"
     click_on "Go"
     click_link 'Add a new product'
     fill_in "Name", :with => 'Cheesecake'
